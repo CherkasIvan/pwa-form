fe0314385b14f6225814feac5516e967d7b6d12b
JV13 <cherkas.ivan13@gmail.com>
2025-03-04 09:37:20 +0300

    workble service-worker

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,28 @@
-import { ApplicationConfig } from '@angular/core';
+import {
+  ApplicationConfig,
+  isDevMode,
+  importProvidersFrom,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
 
+registerLocaleData(en);
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: true,
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+    importProvidersFrom(FormsModule),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+  ],
 };
