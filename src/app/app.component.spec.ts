bee7a7f0f462b1cc8303c897ef781f6f917a9016
JV13 <cherkas.ivan13@gmail.com>
2025-08-02 10:31:03 +0300

    pwa fornm

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'pwa-table' title`, () => {
+  it(`should have the 'pwa-form' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pwa-table');
+    expect(app.title).toEqual('pwa-form');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pwa-table');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, pwa-form'
+    );
   });
 });
